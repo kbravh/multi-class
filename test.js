49aa7595891c11520d8c2656dcfeb2c3132a10cc
Karey Higuera <karey.higuera@gmail.com>
2020-08-02 20:37:25 -0500

    ✅  Add test for multiple class inheritance
    A single test has been added using the baretest testing library. It creates a Person class that
    extends two classes, HasAge and HasName. There is an issue with extending classes that have different
    constructors, but this can be avoided by providing the arguments as an object and destructuring them.

---
+++ test.js
@@ -0,0 +1,40 @@
+const test = require('baretest')('Multi-class inheritance')
+const assert = require('assert')
+const multiclass = require('./index')
+
+class HasAge {
+  constructor({age}){
+    this.age = age
+  }
+
+  getAge(){return this.age}
+}
+
+class HasName {
+  constructor({name}){
+    this.name = name
+  }
+
+  getName(){return this.name}v
+}
+
+class Person extends multiclass(HasAge, HasName) {
+  getNameAndAge(){return `${this.name} is ${this.age} years old.`}
+}
+
+let person = {}
+
+test.before(() => {
+  person = new Person({
+    name: "Leeloo",
+    age: 22
+  })
+})
+
+test('Test multi-class inheritance', () => {
+  assert.strictEqual(person.getName(), "Leeloo")
+  assert.strictEqual(person.getAge(), 22)
+  assert.strictEqual(person.getNameAndAge(), "Leeloo is 22 years old.")
+})
+
+test.run()
