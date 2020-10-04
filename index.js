780b60f9cf79c7ef9c53bc3bd9f1fd989a66bef5
Karey Higuera <karey.higuera@gmail.com>
2020-10-04 09:49:13 -0500

    Add Rome for linting (#4)
    * add rome config
    
    * update readme with linting instructions

--- index.js
+++ index.js
@@ -3,42 +3,50 @@
  * all properties of the various parents to be extended.
  * @param  {...any} classes - All the classes you wish to extend
  */
-const multiclass = (...classes) => {
-  /**
+function multiclass(...classes) {
+	/**
    * Class frame to hold all parent properties.
    */
-  class Class {
-    constructor(...options){
-      // Loop over all incoming classes
-      for (let baseClass of classes){
-        // check if this class already extends another class
-        let parent = Reflect.getPrototypeOf(baseClass)
-        // Push the parent onto the classes array if it's a class and if it isn't yet there
-        if(parent.prototype && !classes.includes(parent)){
-          classes.push(parent)
-        }
-        // Grab a list of the properties attached to the class; methods, variables, &c.
-        const properties = Reflect.ownKeys(baseClass.prototype)
-        // loop through our properties
-        for (let property of properties){
-          // If it's a constructor, we'll pass it our incoming arguments
-          if(property === 'constructor'){
-            Object.assign(Class.prototype, new baseClass.prototype.constructor(...options))
-            // look for static methods or variables on the class
-            let staticProperties = Reflect.ownKeys(baseClass.prototype.constructor).filter(property => !["length", "prototype", "name"].includes(property))
-            // attach these static properties to the class frame
-            for (let staticProperty of staticProperties){
-              Class[staticProperty] = baseClass[staticProperty]
-            }
-          } else { // otherwise, we'll just attach it to our Class frame
-            Class.prototype[property] = baseClass.prototype[property]
-          }
-        }
-      }
-    }
-  }
+	class Class {
+		constructor(...options) {
+			// Loop over all incoming classes
+			for (let baseClass of classes) {
+				// check if this class already extends another class
+				let parent = Reflect.getPrototypeOf(baseClass);
+				// Push the parent onto the classes array if it's a class and if it isn't yet there
+				if (parent.prototype && !classes.includes(parent)) {
+					classes.push(parent);
+				}
+				// Grab a list of the properties attached to the class; methods, variables, &c.
+				const properties = Reflect.ownKeys(baseClass.prototype);
+				// loop through our properties
+				for (let property of properties) {
+					// If it's a constructor, we'll pass it our incoming arguments
+					if (property === "constructor") {
+						Object.assign(
+							Class.prototype,
+							new baseClass.prototype.constructor(...options),
+						);
+						// look for static methods or variables on the class
+						let staticProperties = Reflect.ownKeys(
+							baseClass.prototype.constructor,
+						).filter((property) =>
+							!["length", "prototype", "name"].includes(property)
+						);
+						// attach these static properties to the class frame
+						for (let staticProperty of staticProperties) {
+							Class[staticProperty] = baseClass[staticProperty];
+						}
+					} else {
+						// otherwise, we'll just attach it to our Class frame
+						Class.prototype[property] = baseClass.prototype[property];
+					}
+				}
+			}
+		}
+	}
 
-  return Class
+	return Class;
 }
 
-module.exports = multiclass
+module.exports = multiclass;
